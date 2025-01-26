73229c934dfda2030f4e21209b17649a03f7bb2a
khemraj <khemrajraj@73gmail.com>
2025-01-26 22:45:35 +0530

    Create method and route for register customer account

--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,43 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckBearerToken;
 use App\Http\Controllers\Api\{
     UserController,
-    CompanyController,
+    CustomerController,
     SuperAdminController,
 };
 
-// Super Admin Routes
+/**
+ * Super Admin Routes
+ * These routes are for creating and managing super admin users.
+ */
 Route::controller(SuperAdminController::class)->group(function () {
-    Route::post('create-super-admin', 'createSuperAdmin');
+    Route::post('create-super-admin', 'createSuperAdmin');  // Create a new super admin
 });
 
-// Users Routes
+/**
+ * User Authentication Routes
+ * These routes handle user login, logout, and authentication-related operations.
+ */
 Route::controller(UserController::class)->group(function () {
-    Route::post('login', 'loginUser');
+    Route::post('login', 'loginUser');  // Login user
 });
 
+// Route for logging out the authenticated user, protected by Sanctum authentication.
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('logout', [UserController::class, 'userLogoutRoute']);
+    Route::get('logout', [UserController::class, 'userLogoutRoute']);  // Logout the user
 });
 
-// Company Routes
+/**
+ * Customer Routes
+ * These routes manage customer-related functionalities.
+ */
+Route::controller(CustomerController::class)->group(function () {
+    Route::post('create-customer', 'callCustomer');  // Create a new customer
+});
+
+/**
+ * User Profile Route
+ * This route allows access to the user's profile, protected by a custom Bearer Token middleware.
+ */
 Route::middleware(CheckBearerToken::class)->group(function () {
-    
-    Route::get('profile', [UserController::class, 'userProfileRoute']);
-    Route::controller(CompanyController::class)->group(function () {
-        Route::post('create-company', 'routeCreateCompany');
-    });
+    Route::get('profile', [UserController::class, 'userProfileRoute']);  // Get the authenticated user's profile
 });
