0f54bff74e875904ffce5fe3fe0bc8de2bc5cf59
khemraj <khemrajraj@73gmail.com>
2025-01-26 23:16:53 +0530

    Create method and route for register customer account

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Validator; 
+use Illuminate\Support\Facades\Log; 
+use App\Helpers\ResponseHelper; 
 
 class CustomerController extends Controller
 {
@@ -18,26 +20,34 @@ class CustomerController extends Controller
      */
     protected function createCustomer(Request $request)
     {
-
         // Validate incoming request
-        $passwordRule = passwordRuleFunc();
-        $validated = $request->validate([
-            'name'     => 'required|string|max:255',
-            'email'    => 'required|email|unique:customers,email',
-            'password' => 'required|'.$passwordRule,
+        $passwordRule = passwordRuleFunc(); 
+
+        // Validate the incoming request
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255', 'regex:/^[\pL\s]+$/u'],
+            'email' => ['required', 'string', 'email', 'unique:users', 'regex:/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/'],
+            'password' => ['required', 'confirmed', $passwordRule],
         ]);
 
+        // Check if validation fails
+        if ($validator->fails()) {
+            return ResponseHelper::failResponse($validator->errors(), 422); 
+        }
+
         try {
-       
-            // Create the customer 
+            // Get the validated data
+            $validated = $validator->validated();
+
+            // Create the customer
             $customer = User::create([
-                'name'     => $validated['name'],
-                'email'    => $validated['email'],
-                'password' => Hash::make($validated['password']), 
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
             ]);
 
             // Return a success response
-            return successResponse('Account created successfully!',201);
+            return ResponseHelper::successResponse('Account created successfully!', 201);
 
         } catch (\Exception $e) {
             Log::error('Error creating customer account: ' . $e->getMessage());
